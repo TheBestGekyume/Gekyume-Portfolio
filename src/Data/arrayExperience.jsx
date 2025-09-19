d685f2dcd63c3a58af38272f0308d5abd0932915
Gekyume Serna <contatogekyume@gmail.com>
2025-09-19 12:07:09 -0300

    separa o arrayExperience do componente

---
+++ src/Data/arrayExperience.jsx
@@ -0,0 +1,29 @@
+const arrayExperince = [
+    {
+        title: "Desenvolvimento Colaborativo",
+        description: `O desenvolvimento do projeto freelance foi feito colaborativamente, 
+          com base em conversas diretas, troca constante de ideias e testes práticos.
+          Ao longo do processo, mantive um diálogo aberto com a cliente, garantindo que cada
+          etapa da criação refletisse suas expectativas e necessidades. Por meio do feedback contínuo,
+          ajustamos e aperfeiçoamos cada detalhe, resultando numa solução personalizada.`,
+        side: "left",
+    },
+    {
+        title: "Comunicação e Profissionalismo",
+        description: `Além do desenvolvimento técnico, essa experiência me proporcionou aprendizados
+          valiosos em comunicação. Foi meu primeiro contato direto com um cliente real, o que me ensinou
+          sobre o alinhamento de expectativas, clareza nas entregas e, principalmente, a importância de
+          cumprir prazos e metas em um contexto profissional.`,
+        side: "right",
+    },
+    {
+        title: "Aprofundando nas Tecnologias Web",
+        description: `Durante o projeto, também aprofundei meus conhecimentos em SEO, performance,
+                    acessibilidade e boas práticas de desenvolvimento web. Além disso, refinei
+                    minhas habilidades com React, consolidando tanto aspectos técnicos quanto
+                    interpessoais que foram fundamentais para o sucesso dessa entrega.`,
+        side: "left",
+    },
+];
+
+export default arrayExperince;
